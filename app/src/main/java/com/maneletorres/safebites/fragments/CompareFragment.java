e22492996a157790b98d6c64e46468ad73c6f027
maneletorres <maneletorres@hotmail.com>
2019-04-21 16:39:38 +0200

    Fixed the serious error by which the products are duplicated in CompareFragment in certain situations.

--- app/src/main/java/com/maneletorres/safebites/fragments/CompareFragment.java
+++ app/src/main/java/com/maneletorres/safebites/fragments/CompareFragment.java
@@ -17,21 +17,16 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 import com.maneletorres.safebites.CaptureActivityPortrait;
 import com.maneletorres.safebites.ComparatorActivity;
+import com.maneletorres.safebites.MainActivity;
 import com.maneletorres.safebites.R;
 import com.maneletorres.safebites.entities.Product;
 import com.maneletorres.safebites.utils.AsyncResponse;
 import com.maneletorres.safebites.utils.JsonTask;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -44,10 +39,9 @@ import static com.maneletorres.safebites.utils.Utils.RC_SCAN_OPTION_1_FIRST_EXEC
 import static com.maneletorres.safebites.utils.Utils.RC_SCAN_OPTION_1_SECOND_EXECUTION;
 import static com.maneletorres.safebites.utils.Utils.RC_SCAN_OPTION_2;
 import static com.maneletorres.safebites.utils.Utils.TAIL_SPECIFIC_PRODUCT_URL;
-import static com.maneletorres.safebites.utils.Utils.sUser;
+import static com.maneletorres.safebites.utils.Utils.sProducts;
 
-public class ComparatorFragment extends Fragment implements View.OnClickListener, AsyncResponse {
-    private Fragment mFragment;
+public class CompareFragment extends Fragment implements View.OnClickListener, AsyncResponse, MainActivity.MyInterface {
     private LinearLayout mProductAContainer;
     private LinearLayout mProductBContainer;
     private Button mScanButton;
@@ -55,8 +49,7 @@ public class ComparatorFragment extends Fragment implements View.OnClickListener
     private TextView mScanTextView;
     private Spinner mProductASpinner;
     private Spinner mProductBSpinner;
-    private List<Product> favoriteProducts;
-    private ArrayAdapter<Product> adapterFavoriteProducts;
+    private List<Product> mFavoriteProducts;
     private int mOption;
     private int request_code;
     private Product mProductA;
@@ -66,91 +59,52 @@ public class ComparatorFragment extends Fragment implements View.OnClickListener
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_comparator, container, false);
 
-        // Firebase Realtime Database components initialization:
-        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("users").child(sUser.getUser_id()).child("products");
-        ChildEventListener childEventListener = new ChildEventListener() {
-            @Override
-            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-                Product product = dataSnapshot.getValue(Product.class);
-                favoriteProducts.add(product);
-                adapterFavoriteProducts.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-            }
-
-            @Override
-            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
-                Product product = dataSnapshot.getValue(Product.class);
-                if (product != null) {
-                    favoriteProducts.remove(product);
-                    adapterFavoriteProducts.notifyDataSetChanged();
-                }
-            }
-
-            @Override
-            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        };
-
-        // Linking the ChildEventListener to the DatabaseReference:
-        databaseReference.addChildEventListener(childEventListener);
-
         // Initialization of the components:
-        mFragment = this;
         mProductAContainer = view.findViewById(R.id.product_A_container);
         mProductASpinner = view.findViewById(R.id.product_A_spinner);
         mProductBContainer = view.findViewById(R.id.product_B_container);
         mProductBSpinner = view.findViewById(R.id.product_B_spinner);
         mScanProgressBar = view.findViewById(R.id.scan_progress_bar);
         mScanTextView = view.findViewById(R.id.scan_text);
-        mScanButton = view.findViewById(R.id.scan_button);
-        mScanButton.setOnClickListener(this);
-
-        favoriteProducts = new ArrayList<>();
-        adapterFavoriteProducts = new ArrayAdapter<>(Objects.requireNonNull(getContext()), android.R.layout.simple_spinner_dropdown_item, favoriteProducts);
-        adapterFavoriteProducts.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        mProductASpinner.setAdapter(adapterFavoriteProducts);
-        mProductBSpinner.setAdapter(adapterFavoriteProducts);
-
+        mScanButton = view.findViewById(R.id.compare_button);
         RadioGroup radioGroup = view.findViewById(R.id.options_radio_group);
-        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup radioGroup, int i) {
-                switch (radioGroup.getCheckedRadioButtonId()) {
-                    case R.id.radio_button_1:
-                        mOption = 1;
-                        mProductAContainer.setVisibility(View.GONE);
-                        mProductBContainer.setVisibility(View.GONE);
-                        break;
-                    case R.id.radio_button_2:
-                        mOption = 2;
-                        mProductAContainer.setVisibility(View.VISIBLE);
-                        mProductBContainer.setVisibility(View.VISIBLE);
-                        break;
-                    case R.id.radio_button_3:
-                        mOption = 3;
-                        mProductAContainer.setVisibility(View.VISIBLE);
-                        mProductBContainer.setVisibility(View.GONE);
-                        break;
-                }
+
+        // Loading the names of the products:
+        prepareProductsNamesLoading();
+
+        // OnCheckedChangeListener configuration on the RadioGroup and selection of the second radio
+        // button by default:
+        mOption = 2;
+        radioGroup.check(R.id.radio_button_2);
+        radioGroup.setOnCheckedChangeListener((rg, i) -> {
+            switch (rg.getCheckedRadioButtonId()) {
+                case R.id.radio_button_1:
+                    mOption = 1;
+                    mProductAContainer.setVisibility(View.GONE);
+                    mProductBContainer.setVisibility(View.GONE);
+                    break;
+                case R.id.radio_button_2:
+                    mOption = 2;
+                    mProductAContainer.setVisibility(View.VISIBLE);
+                    mProductBContainer.setVisibility(View.VISIBLE);
+                    break;
+                case R.id.radio_button_3:
+                    mOption = 3;
+                    mProductAContainer.setVisibility(View.VISIBLE);
+                    mProductBContainer.setVisibility(View.GONE);
+                    break;
             }
         });
+
+        // OnClickListener configuration on the button to scan products:
+        mScanButton.setOnClickListener(this);
 
         return view;
     }
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.scan_button) {
+        if (v.getId() == R.id.compare_button) {
             switch (mOption) {
                 case 1:
                     startComparisonOption1(RC_SCAN_OPTION_1_FIRST_EXECUTION);
@@ -159,7 +113,11 @@ public class ComparatorFragment extends Fragment implements View.OnClickListener
                     startComparisonOption2();
                     break;
                 case 3:
-                    startComparisonOption1(RC_SCAN_OPTION_2);
+                    if (mProductASpinner.getCount() > 0) {
+                        startComparisonOption1(RC_SCAN_OPTION_2);
+                    } else {
+                        Toast.makeText(getContext(), "There is no product saved as favorite to make the comparison", Toast.LENGTH_SHORT).show();
+                    }
                     break;
             }
         }
@@ -175,20 +133,28 @@ public class ComparatorFragment extends Fragment implements View.OnClickListener
             case RC_SCAN_OPTION_2:
                 IntentResult scanResult = parseActivityResult(IntentIntegrator.REQUEST_CODE, resultCode, data);
                 if (scanResult != null) {
-                    if (scanResult.getContents() == null || resultCode == RESULT_CANCELED) {
-                        Toast.makeText(getContext(), "Scan canceled by the user", Toast.LENGTH_SHORT).show();
+                    if (scanResult.getContents() == null) {
+                        if (resultCode == RESULT_CANCELED) {
+                            Toast.makeText(getContext(), "Scan canceled by the user", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(getContext(), "Error during scanning.", Toast.LENGTH_SHORT).show();
+                        }
                     } else {
                         startScan(scanResult.getContents());
                     }
                 } else {
+                    Toast.makeText(getContext(), "Error during scanning.", Toast.LENGTH_SHORT).show();
                     super.onActivityResult(requestCode, resultCode, data);
                 }
-                break;
         }
     }
 
     @Override
     public void processFinish(Product output) {
+        mScanProgressBar.setVisibility(View.GONE);
+        mScanTextView.setVisibility(View.GONE);
+        mScanButton.setVisibility(View.VISIBLE);
+
         if (output != null) {
             switch (request_code) {
                 case RC_SCAN_OPTION_1_FIRST_EXECUTION:
@@ -206,26 +172,17 @@ public class ComparatorFragment extends Fragment implements View.OnClickListener
                     break;
                 case RC_SCAN_OPTION_2:
                     mProductA = output;
+                    productB = (Product) mProductASpinner.getSelectedItem();
 
-                    if (mProductASpinner.getCount() > 0) {
-                        productB = (Product) mProductASpinner.getSelectedItem();
-
-                        intent = new Intent(getContext(), ComparatorActivity.class);
-                        intent.putExtra(PRODUCT_A, mProductA);
-                        intent.putExtra(PRODUCT_B, productB);
-                        startActivity(intent);
-                    } else {
-                        Toast.makeText(getContext(), "There are not enough products saved as a favorite to make the comparison", Toast.LENGTH_SHORT).show();
-                    }
+                    intent = new Intent(getContext(), ComparatorActivity.class);
+                    intent.putExtra(PRODUCT_A, mProductA);
+                    intent.putExtra(PRODUCT_B, productB);
+                    startActivity(intent);
                     break;
             }
         } else {
             Toast.makeText(getContext(), "Product not found", Toast.LENGTH_SHORT).show();
         }
-
-        mScanProgressBar.setVisibility(View.GONE);
-        mScanTextView.setVisibility(View.GONE);
-        mScanButton.setVisibility(View.VISIBLE);
     }
 
     private void startScan(String scanResult) {
@@ -235,13 +192,13 @@ public class ComparatorFragment extends Fragment implements View.OnClickListener
         mScanProgressBar.setVisibility(View.VISIBLE);
         mScanTextView.setVisibility(View.VISIBLE);
 
-        JsonTask jsonTask = new JsonTask(Objects.requireNonNull(getContext()));
+        JsonTask jsonTask = new JsonTask();
         jsonTask.delegate = this;
         jsonTask.execute(queryUrl);
     }
 
     private void startComparisonOption1(int requestCode) {
-        IntentIntegrator.forSupportFragment(mFragment)
+        IntentIntegrator.forSupportFragment(this)
                 .setRequestCode(requestCode)
                 .setPrompt("Scan a product's barcode")
                 .setCameraId(0)
@@ -260,5 +217,21 @@ public class ComparatorFragment extends Fragment implements View.OnClickListener
         } else {
             Toast.makeText(getContext(), "There are not enough products saved as a favorite to make the comparison", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    // IMPORTANT: this method can be executed before the onCreateView method is executed.
+    @Override
+    public void updateProducts() {
+        if (mFavoriteProducts != null) {
+            prepareProductsNamesLoading();
+        }
+    }
+
+    private void prepareProductsNamesLoading() {
+        mFavoriteProducts = sProducts;
+        ArrayAdapter<Product> adapterFavoriteProducts = new ArrayAdapter<>(Objects.requireNonNull(getContext()), android.R.layout.simple_spinner_dropdown_item, mFavoriteProducts);
+        adapterFavoriteProducts.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mProductASpinner.setAdapter(adapterFavoriteProducts);
+        mProductBSpinner.setAdapter(adapterFavoriteProducts);
     }
 }
