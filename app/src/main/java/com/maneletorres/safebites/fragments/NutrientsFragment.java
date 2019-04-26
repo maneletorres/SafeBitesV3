a73d3d30c1f5a01cdc01b712bc0ae349d9715ef7
maneletorres <maneletorres@hotmail.com>
2019-04-27 01:09:21 +0200

    Improved code efficiency and elimination of redundant elements of the class 'NutrientsFragment'.

--- app/src/main/java/com/maneletorres/safebites/fragments/NutrientsFragment.java
+++ app/src/main/java/com/maneletorres/safebites/fragments/NutrientsFragment.java
@@ -27,21 +27,20 @@ import com.google.firebase.database.ValueEventListener;
 import com.maneletorres.safebites.R;
 import com.maneletorres.safebites.adapters.NutrientAdapter;
 import com.maneletorres.safebites.entities.Product;
-import com.maneletorres.safebites.utils.Utils;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
 import static com.maneletorres.safebites.utils.Utils.PRODUCT;
+import static com.maneletorres.safebites.utils.Utils.sUID;
+import static com.maneletorres.safebites.utils.Utils.sUser;
 
 public class NutrientsFragment extends Fragment implements View.OnClickListener {
-    // Firebase Realtime Database components:
+    // FRDB variables:
     private DatabaseReference mProductsDatabaseReference;
-    private DatabaseReference mUserDatabaseReference;
 
-    // Components:
+    // Other variables:
     private FloatingActionButton mSaveOrDeleteFAB;
     private Product mProduct;
     private boolean mProductExists;
@@ -54,86 +53,123 @@ public class NutrientsFragment extends Fragment implements View.OnClickListener 
         Bundle extras = getArguments();
         if (extras != null) {
             mProduct = extras.getParcelable(PRODUCT);
-            if (mProduct != null) {
-                // Initialization of the components:
-                ImageView image_nutrients = view.findViewById(R.id.image_view_nutrients_header);
-                TextView name = view.findViewById(R.id.product_name_text_view);
-                TextView upc = view.findViewById(R.id.product_upc_text_view);
-                TextView header_per_serving = view.findViewById(R.id.header_per_serving);
-                TextView apt_product = view.findViewById(R.id.apt_product_text_view);
-                mSaveOrDeleteFAB = view.findViewById(R.id.save_or_delete_FAB);
 
-                NutrientAdapter nutrientAdapter = new NutrientAdapter(mProduct.getNutrients());
-                RecyclerView recyclerView = view.findViewById(R.id.nutrient_recycler);
-                recyclerView.setAdapter(nutrientAdapter);
+            // Initialization of the components:
+            ImageView image_nutrients = view.findViewById(R.id.image_view_nutrients_header);
+            TextView name = view.findViewById(R.id.product_name_text_view);
+            TextView upc = view.findViewById(R.id.product_upc_text_view);
+            TextView apt_product = view.findViewById(R.id.apt_product_text_view);
+            TextView header_per_100g = view.findViewById(R.id.header_per_100g);
+            TextView header_per_serving = view.findViewById(R.id.header_per_serving);
+            mSaveOrDeleteFAB = view.findViewById(R.id.save_or_delete_FAB);
 
-                // FAB OnClickListener:
-                mSaveOrDeleteFAB.setOnClickListener(this);
+            // Initialization of the FRDB components:
+            //DatabaseReference userDatabaseReference = FirebaseDatabase.getInstance().getReference("users").child(sUID);
+            mProductsDatabaseReference = FirebaseDatabase.getInstance().getReference("users").child(sUID).child("products");
 
-                // Initialization of the Firebase Realtime Database components:
-                FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+            // Listener that checks if the product exists in the Firebase Realtime Database to
+            // modify accordingly 'mSaveOrDeleteFAB':
+            mProductsDatabaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    String upc = mProduct.getUpc();
+                    if (snapshot.hasChild(upc)) {
+                        mSaveOrDeleteFAB.setImageResource(R.drawable.delete);
+                        mSaveOrDeleteFAB.setContentDescription("Delete ImageButton");
+                        mProductExists = true;
+                    } else {
+                        mSaveOrDeleteFAB.setImageResource(R.drawable.content_save);
+                        mSaveOrDeleteFAB.setContentDescription("Save ImageButton");
+                        mProductExists = false;
+                    }
+                }
 
-                mUserDatabaseReference = firebaseDatabase.getReference("users").child(Utils.sUser.getUser_id()).child("allergies");
-                mUserDatabaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        ArrayList<String> productAllergens = mProduct.getAllergens();
-                        if(productAllergens != null){
-                            HashMap<String, Boolean> userAllergens = (HashMap<String, Boolean>) dataSnapshot.getValue();
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
 
-                            boolean condition = false;
-                            for(Map.Entry<String, Boolean> entry : userAllergens.entrySet()) {
+                }
+            });
+
+            /*DatabaseReference mAllergiesDatabaseReference = userDatabaseReference.child("allergies");
+            mAllergiesDatabaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    ArrayList<String> productAllergens = mProduct.getAllergens();
+                    if (productAllergens != null) {
+                        HashMap<String, Boolean> userAllergens = (HashMap<String, Boolean>) dataSnapshot.getValue();
+
+                        boolean condition = false;
+                        for (Map.Entry<String, Boolean> entry : userAllergens.entrySet()) {
+                            if (!condition) {
                                 String key = entry.getKey();
                                 Boolean value = entry.getValue();
-                                Log.v("USER ALLERGY", key + " " + value);
 
-                                for(int i = 0; i < productAllergens.size() && !condition; i++){
+                                for (int i = 0; i < productAllergens.size() && !condition; i++) {
                                     String allergy = productAllergens.get(i);
-                                    Log.v("PRODUCT ALLERGY", allergy);
-                                    if(allergy.equals(key) && value){
-                                        Log.v("unfit","unfit");
+                                    if (allergy.equals(key) && value) {
                                         condition = true;
-                                        apt_product.setText("Unfit");
-                                        apt_product.setBackgroundColor(Color.parseColor("#FF0000"));
-                                    } else {
-                                        Log.v("suitable","suitable");
-                                        apt_product.setText("Suitable");
-                                        apt_product.setBackgroundColor(Color.parseColor("#ff99cc00"));
                                     }
+                                }
+                            } else {
+                                break;
+                            }
+                        }
+
+                        if (condition) {
+                            apt_product.setText("Unfit");
+                            apt_product.setBackgroundColor(Color.parseColor("#FF0000"));
+                        } else {
+                            apt_product.setText("Suitable");
+                            apt_product.setBackgroundColor(Color.parseColor("#ff99cc00"));
+                        }
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });*/
+
+            // Loading of nutrients:
+            NutrientAdapter nutrientAdapter = new NutrientAdapter(mProduct.getNutrients());
+            RecyclerView recyclerView = view.findViewById(R.id.nutrient_recycler);
+            recyclerView.setAdapter(nutrientAdapter);
+
+            // OnClickListener configuration on the FAB to save or delete a product as a favorite:
+            mSaveOrDeleteFAB.setOnClickListener(this);
+
+            // Allergy check:
+            boolean condition = false;
+            Map<String, Boolean> userAllergies = sUser.getAllergies();
+            if (userAllergies != null) {
+                for (Map.Entry<String, Boolean> entry : userAllergies.entrySet()) {
+                    if (!condition) {
+                        String key = entry.getKey();
+                        Boolean value = entry.getValue();
+
+                        ArrayList<String> productAllergies = mProduct.getAllergens();
+                        if (productAllergies != null) {
+                            for (int i = 0; i < productAllergies.size() && !condition; i++) {
+                                String allergy = productAllergies.get(i);
+                                if (allergy.equals(key) && value) {
+                                    condition = true;
                                 }
                             }
                         }
+                    } else {
+                        break;
                     }
+                }
+            }
 
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                    }
-                });
-
-                mProductsDatabaseReference = firebaseDatabase.getReference("users").child(Utils.sUser.getUser_id()).child("products");
-                // Listener that checks if the product exists in the Firebase Realtime Database to
-                // modify accordingly 'mSaveOrDeleteFAB':
-                mProductsDatabaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot snapshot) {
-                        String upc = mProduct.getUpc();
-                        if (snapshot.hasChild(upc)) {
-                            mSaveOrDeleteFAB.setImageResource(R.drawable.delete);
-                            mSaveOrDeleteFAB.setContentDescription("Delete ImageButton");
-                            mProductExists = true;
-                        } else {
-                            mSaveOrDeleteFAB.setImageResource(R.drawable.content_save);
-                            mSaveOrDeleteFAB.setContentDescription("Save ImageButton");
-                            mProductExists = false;
-                        }
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                    }
-                });
+            if (condition) {
+                apt_product.setText("Unfit");
+                apt_product.setBackgroundColor(Color.parseColor("#FF0000"));
+            } else {
+                apt_product.setText("Suitable");
+                apt_product.setBackgroundColor(Color.parseColor("#ff99cc00"));
+            }
 
             // Loading the information in the elements of the layout:
             String image_resource = mProduct.getImage_resource();
