917ea2270b9009ee60e15b959ca4a43763df8d37
maneletorres <maneletorres@hotmail.com>
2019-04-29 19:05:10 +0200

    Adaptation of the 'ProductActivity' class and the XML file 'activity_product' to devices with large screens.

--- app/src/main/java/com/maneletorres/safebites/ProductActivity.java
+++ app/src/main/java/com/maneletorres/safebites/ProductActivity.java
@@ -16,6 +16,7 @@ import java.util.Objects;
 
 import static com.maneletorres.safebites.utils.Utils.INGREDIENTS;
 import static com.maneletorres.safebites.utils.Utils.PRODUCT;
+import static com.maneletorres.safebites.utils.Utils.TWO_PANE;
 
 public class ProductActivity extends AppCompatActivity {
     private Bundle mBundle;
@@ -32,15 +33,35 @@ public class ProductActivity extends AppCompatActivity {
 
         mBundle = getIntent().getExtras();
         if (mBundle != null) {
+            boolean twoPane = mBundle.getBoolean(TWO_PANE);
             mProduct = mBundle.getParcelable(PRODUCT);
+
+            if (!twoPane) {
+                // Initialization of the components:
+                ViewPager mViewPager = findViewById(R.id.viewPagerContainer);
+                setupViewPager(mViewPager);
+
+                TabLayout tabLayout = findViewById(R.id.tabs);
+                tabLayout.setupWithViewPager(mViewPager);
+            } else {
+                Bundle bundle1 = new Bundle();
+                bundle1.putParcelable(PRODUCT, mProduct);
+
+                NutrientsFragment nutrientsFragments = new NutrientsFragment();
+                nutrientsFragments.setArguments(mBundle);
+
+                Bundle bundle2 = new Bundle();
+                bundle2.putString(INGREDIENTS, mProduct.getIngredients());
+                IngredientsFragment ingredientsFragment = new IngredientsFragment();
+                ingredientsFragment.setArguments(bundle2);
+
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .add(R.id.product_frame_layout_1, nutrientsFragments)
+                        .add(R.id.product_frame_layout_2, ingredientsFragment)
+                        .commit();
+            }
         }
-
-        // Initialization of the components:
-        ViewPager mViewPager = findViewById(R.id.viewPagerContainer);
-        setupViewPager(mViewPager);
-
-        TabLayout tabLayout = findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(mViewPager);
     }
 
     @Override
